9789204b2b45f25df5e56193b53c15dc8f714328
wvaske <wvaske@micron.com>
2024-11-21 11:30:05 -0700

    argument parsing matches original benchmark.sh

--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,8 @@
 import argparse
 import sys
 
+from mlperf_logging import mllog
+
 # Define constants:
 COSMOFLOW = "cosmoflow"
 RESNET = "resnet50"
@@ -23,6 +25,14 @@ CATEGORIES = [OPEN, CLOSED]
 #
 # Configure datasize to collect the memory information from the hosts instead of getting a number of hosts for the
 #   calculation
+#
+# Add logging module for better control of output messages
+#
+# Add function to generate DLIO command and manage execution
+#
+# Determine method to use cgroups for memory limitation in the benchmark script.
+#
+# Add a log block at the start of datagen & run that output all the parms being used to be clear on what a run is.
 
 
 def parse_arguments():
@@ -59,15 +69,16 @@ def parse_arguments():
 
     parser = argparse.ArgumentParser(description="Script to launch the MLPerf Storage benchmark")
     subparsers = parser.add_subparsers(dest="command", required=True, help="Sub-commands")
+    subparsers.required = True
 
-    datasize = subparsers.add_parser("datasize", description=help_messages['datasize'])
+    datasize = subparsers.add_parser("datasize", help=help_messages['datasize'])
     datasize.add_argument('--workload', '-w', choices=WORKLOADS, help=help_messages['workload'])
     datasize.add_argument('--accelerator-type', '-g', choices=ACCELERATORS, help=help_messages['accelerator_type'])
     datasize.add_argument('--num-accelerators', '-n', type=int, help=help_messages['num_accelerators_datasize'])
     datasize.add_argument('--num-client-hosts', '-c', type=int, help=help_messages['num_client_hosts'])
     datasize.add_argument('--client-host-memory-in-gb', '-m', type=int, help=help_messages['client_host_mem_GB'])
 
-    datagen = subparsers.add_parser("datagen", description=help_messages['datagen'])
+    datagen = subparsers.add_parser("datagen", help=help_messages['datagen'])
     datagen.add_argument('--hosts', '-s', type=str, help=help_messages['client_hosts'])
     datagen.add_argument('--category', '-c', choices=CATEGORIES, help=help_messages['category'])
     datagen.add_argument('--workload', '-w', choices=WORKLOADS, help=help_messages['workload'])
@@ -76,7 +87,7 @@ def parse_arguments():
     datagen.add_argument('--results-dir', '-r', type=str, help=help_messages['results_dir'])
     datagen.add_argument('--param', '-p', action="append", nargs="+", type=str, help=help_messages['param'])
 
-    run_benchmark = subparsers.add_parser("run", description=help_messages['run_benchmark'])
+    run_benchmark = subparsers.add_parser("run", help=help_messages['run_benchmark'])
     run_benchmark.add_argument('--hosts', '-s', type=str, help=help_messages['client_hosts'])
     run_benchmark.add_argument('--category', '-c', choices=CATEGORIES, help=help_messages['category'])
     run_benchmark.add_argument('--workload', '-w', choices=WORKLOADS, help=help_messages['workload'])
@@ -85,12 +96,12 @@ def parse_arguments():
     run_benchmark.add_argument('--results-dir', '-r', type=str, help=help_messages['results_dir'])
     run_benchmark.add_argument('--param', '-p', action="append", nargs="+", type=str, help=help_messages['param'])
 
-    configview = subparsers.add_parser("configview", description=help_messages['configview'])
+    configview = subparsers.add_parser("configview", help=help_messages['configview'])
     configview.add_argument('--workload', '-w', choices=WORKLOADS, help=help_messages['workload'])
     configview.add_argument('--accelerator-type', '-g', choices=ACCELERATORS, help=help_messages['accelerator_type'])
     configview.add_argument('--param', '-p', action="append", nargs="+", type=str, help=help_messages['param'])
 
-    reportgen = subparsers.add_parser("reportgen", description=help_messages['reportgen'])
+    reportgen = subparsers.add_parser("reportgen", help=help_messages['reportgen'])
     reportgen.add_argument('--results-dir', '-r', type=str, help=help_messages['results_dir'])
 
     return parser.parse_args()
@@ -105,17 +116,50 @@ def validate_args(args):
         for msg in error_messages:
             print(msg)
 
-    sys.exit(1)
+        sys.exit(1)
 
 
+def datasize(args):
+    logger.event(f'Got to datasize', metadata=vars(args))
+
+
+def datagen(args):
+    logger.event(f'Got to datagen', metadata=vars(args))
+
+
+def run_benchmark(args):
+    logger.event(f'Got to run_benchmark', metadata=vars(args))
+
+
+def configview(args):
+    logger.event(f'Got to configview', metadata=vars(args))
+
+
+def reportgen(args):
+    logger.event(f'Got to reportgen', metadata=vars(args))
+
+
+# Main function to handle command-line arguments and invoke the corresponding function.
 def main(args):
-    print(f'Got to main')
-    import pprint
-    pprint.pprint(vars(args))
-
+    logger.event(f'Got to main')
     validate_args(args)
+
+    # The command arg tells us which command func to run
+    command_switch_dict = dict(
+        datasize=datasize,
+        datagen=datagen,
+        run=run_benchmark,
+        configview=configview,
+        reportgen=reportgen,
+    )
+
+    # Call the specific function for a given command
+    command_switch_dict[args.command](args)
 
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    main(args)
+    # Get the logger and args. Call main to run program
+    logger = mllog.get_mllogger()
+    cli_args = parse_arguments()
+    main(cli_args)
+
