4fd8cf2e5f430f57a949868ef0fcaa89be791c5c
wvaske <wvaske@dev.local>
2026-01-24 05:09:53 +0000

    feat(05-01): export VectorDBRunRulesChecker from run_checkers
    - Add import for VectorDBRunRulesChecker
    - Add to __all__ exports list

--- mlpstorage/rules/run_checkers/__init__.py
+++ mlpstorage/rules/run_checkers/__init__.py
@@ -8,10 +8,12 @@ from mlpstorage.rules.run_checkers.base import RunRulesChecker
 from mlpstorage.rules.run_checkers.training import TrainingRunRulesChecker
 from mlpstorage.rules.run_checkers.checkpointing import CheckpointingRunRulesChecker
 from mlpstorage.rules.run_checkers.kvcache import KVCacheRunRulesChecker
+from mlpstorage.rules.run_checkers.vectordb import VectorDBRunRulesChecker
 
 __all__ = [
     'RunRulesChecker',
     'TrainingRunRulesChecker',
     'CheckpointingRunRulesChecker',
     'KVCacheRunRulesChecker',
+    'VectorDBRunRulesChecker',
 ]
